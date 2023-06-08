7f9dbe390ef4cd96d65b66b01063181ed44c81b3
Raj Chaudhary <craj68168@gmail.com>
2023-06-08 15:44:33 +0545

    feat:implementing ck editor

---
+++ src/app/components/Editor.js
@@ -0,0 +1,31 @@
+import { Editor as ClassicEditor } from "ckeditor5-custom-build/build/ckeditor";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import React from "react";
+import styled from "styled-components";
+const Wrapper = styled.div``;
+
+const Editor = () => {
+  return (
+    <Wrapper>
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p>Hello from ckeditor5</p>"
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log("onchange ckeditor", { event, editor, data });
+        }}
+        onReady={editor => {
+          console.log("onReady ckeditor", editor);
+        }}
+        onBlur={(event, editor) => {
+          console.log("onBlur ckeditor", { event, editor });
+        }}
+        onFocus={(event, editor) => {
+          console.log("onFocus ckeditor", { event, editor });
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+export default Editor;
